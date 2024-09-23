09d09adf6456222d03fbdec1e5a58c158d7474bb
femi babayemi <femi.babayemi@flutterwavego.com>
2024-09-23 12:25:16 +0100

    photo feature

---
+++ src/main/java/com/femibabs/photoz/clone/repository/PhotozRepository.java
@@ -0,0 +1,8 @@
+package com.femibabs.photoz.clone.repository;
+
+import com.femibabs.photoz.clone.model.Photo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PhotozRepository extends CrudRepository<Photo, Integer> {
+
+}
